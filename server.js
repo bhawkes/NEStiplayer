85e31da36846646aa53a9ee658b6a84c421d242c
Brandon Hawkes <brandon@bhawkes.com>
2014-04-05 13:09:55 +0100

    added some basic client connection code

--- server.js
+++ server.js
@@ -4,12 +4,46 @@ var express = require('express'),
 	io = require('socket.io').listen(server),
     mac_control = require("mac-control");
 
-	server.listen(3000);
+server.listen(3000);
 
-    io.set( 'log level', 1 );
+io.set( 'log level', 1 );
 
-	app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
-	app.get('/',function(req,res){
-		res.sendFile('index.html');
-	});
+app.get('/',function(req,res){
+    res.sendFile('index.html');
+});
+
+var playerCount = 0;
+var players = {};
+var state = {};
+
+io.sockets.on('connection', function(socket){
+        
+    playerCount++;
+    
+    players[socket.id] = {
+        keys:{
+            "up":false,
+            "down":false,
+            "left":false,
+            "right":false,
+            "a":false,
+            "b":false,
+            "start":false,
+            "select":false
+        }
+    };
+
+    socket.on('keys', function(data){
+        players[socket.id].keys = data;
+        console.log(data);
+    });
+
+    socket.on('disconnect', function(){
+        delete players[socket.id];
+        playerCount--;
+    });
+
+});
+
