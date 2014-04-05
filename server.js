9d805188964cd330055c9c408a3c2201e2691bd3
Brandon Hawkes <brandon@bhawkes.com>
2014-04-05 14:25:02 +0100

    test button

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
-    mac_control = require("mac-control");
+    mc = require("mac-control");
 
 server.listen(3000);
 
@@ -36,8 +36,10 @@ io.sockets.on('connection', function(socket){
     };
 
     socket.on('keys', function(data){
+        
         players[socket.id].keys = data;
         console.log(data);
+        updateKeys();
     });
 
     socket.on('disconnect', function(){
@@ -47,3 +49,40 @@ io.sockets.on('connection', function(socket){
 
 });
 
+
+function updateKeys(){
+    // set counts to 0
+    var upCount = 0;
+    var downCount = 0;
+    var leftCount = 0;
+    var rightCount = 0;
+    var aCount = 0;
+    var bCount = 0;
+    var startCount = 0;
+    var selectCount = 0;
+        
+    // then count each player pressing a button
+    for ( var id in players) {
+            
+        var player = players[id];
+            
+        if(player.keys.up) upCount++;
+        if(player.keys.down) downCount++;
+        if(player.keys.left) leftCount++;
+        if(player.keys.right) rightCount++;
+        if(player.keys.a) aCount++;
+        if(player.keys.b) bCount++;
+        if(player.keys.start) startCount++;
+        if(player.keys.select) selectCount++;
+                	
+    }
+    
+    
+    // then set game keys 
+    if(aCount==1){
+        mc.keyHold("x");
+        setInterval(function(){
+            mc.keyRelease("x");
+        },500);
+    }
+}
